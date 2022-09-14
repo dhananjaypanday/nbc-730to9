41d4b6168263f532876702e1a9bfcbabff6304fe
Dhananjay Panday <30781583+dhananjaypanday@users.noreply.github.com>
2022-09-14 08:58:46 +0530

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,33 @@
+var a = 10;
+undefined
+a
+10
+a='Ram'
+'Ram'
+a
+'Ram'
+var arr =[1,2,3,45]
+undefined
+arr
+(4) [1, 2, 3, 45]
+var obj = {id:101, name:'Ram"}
+VM249:1 Uncaught SyntaxError: Invalid or unexpected token
+var obj = {id:101, name:'Ram'}
+undefined
+obj
+{id: 101, name: 'Ram'}
+function myfunction(){
+    console.log('Hello World');
+}
+undefined
+myfunction();
+VM382:2 Hello World
+undefined
+a instanceof Object
+false
+typeof a
+'string'
+typeof arr
+'object'
+arr instanceof Array
+true
